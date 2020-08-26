3a615a5da8903d1224f839e55a8dd9c1d2d82a43
Zong Xian <zongxian96@gmail.com>
2020-08-26 18:54:43 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,31 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+        greeting();
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        while (!str.equals("bye")){
+            System.out.println(str);
+            str = sc.nextLine();
+        }
+
+        bye();
+
+    }
+    public static void lines() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greeting(){
+        lines();
         System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you?");
+        lines();
+    }
+    public static void bye() {
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
